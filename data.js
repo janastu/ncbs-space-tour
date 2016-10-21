7ba3cdd1d28b3fd392e6bcc548e28a55c3cd3a7f
salus-sage <salus.sage@gmail.com>
2016-10-21 20:56:01 +0530

    bug fix: audio player-global

--- data.js
+++ data.js
@@ -3,117 +3,126 @@ var data = [
 		id: 'lookout',
 		title: 'On the lookout for Trainee Students',
 		description: 'Soon after he moved to NCBS, Obaid Siddiqi started getting interested in hiring students at the BSc and MSc level in his lab. Students from the time would discuss the disproportionate size of his lab, compared to other labs. The trainees would stay for three months to a few years, each getting a glimpse of a research life that would otherwise remain unknown to them.',
-		urls: [
-		'content/office/deepti_labtour-trainee_SPACE-TOUR.mp3',
-		'content/office/2005 Vijay to NCBS - OS BSc student hiring.jpg'
-		
-		] 
+		urls: {
+		audio: 'content/office/deepti_labtour-trainee_SPACE-TOUR.mp3',
+		img: ['content/office/2005 Vijay to NCBS - OS BSc student hiring.jpg']
+		}
+		 
 	},
 	{
 		id: 'reprints',
 		title: 'The reprint pile',
 		description: 'Back in the day when it wasnt easy to just print copies of journal articles off the web, reprints carried a lot of currency. Obaid Siddiqi continued to maintain a pile of reprints in his office and an even bigger set in his lab, in alphabetical order of main author. He would then loan them out to new students. Deepti Trivedi recounts.',
-		urls: [
-		'content/office/deepti_labtour-reprints_SPACE-TOUR.mp3'
-		] 
+		urls: {
+		audio: 'content/office/deepti_labtour-reprints_SPACE-TOUR.mp3'
+	}
+		
 	},
 	{
 		id: 'wind',
 		title: 'Fly behaviour in a wind tunnel',
 		description: 'The aim of the wind-tunnel apparatus was to generate streamlined flow of known specific velocities and then possibly measure the behavior of the fly in the wind tunnel. Deepti Trivedi shares her memory of the device and the people who worked on it.',
-		urls: [
-		'content/1/deepti_labtour-windT_SPACE-TOUR.mp3',
-		'content/1/OS Lab - Wind Tunnel - 2.jpg'
-		] 
+		urls: {
+		audio: 'content/1/deepti_labtour-windT_SPACE-TOUR.mp3',
+		img: ['content/1/OS Lab - Wind Tunnel - 2.jpg']
+	}
+		 
 	},
 	{
 		id: 'patch',
 		title: 'Patch clamp lab',
 		description: 'Deepti Trivedi remarks upon seeing her handwriting on a white board and above a set of cables, more than a decade after she passed through the lab. She also notices the handwriting of Obaid Siddiqi. Her comments about her work in the lab: "During the time we were trying to find different ion channels that are present in Drosophila neurons at different stages of development. These were acute cultures of Drosophila larval and adult neurons kept in dishes for specific amounts of time. The cells were patched by the patch pipettes where the glass of the pipette forms a high resistance seal with the cell membrane so that no current can leak through the seal. This ensures that whatever current that is recorded by the patch pipette is a result of the channels that are present in the piece of membrane around which the high resistance seal is formed. If one destroys that piece of membrane by force, then the pipette is able access the whole cell and hence the currents then measured represent the combined activity of the whole cell." ',
-		urls: [
-		'content/1/deepti_labtour-patch-clamp-wires_SPACE-TOUR.mp3',
-		'content/1/OS Lab - Whiteboard scribbles - 2.jpg',
+		urls: {
+		audio: 'content/1/deepti_labtour-patch-clamp-wires_SPACE-TOUR.mp3',
+		img: ['content/1/OS Lab - Whiteboard scribbles - 2.jpg',
 		'content/1/OS Lab - Deepti wires.jpg',
-		'content/1/OS Lab - Cables repo.jpg'
+		'content/1/OS Lab - Cables repo.jpg']
+	}		
 		
-		
-		] 
 	},
 	{
 		id: 'staff',
 		title: 'Interaction with support staff in Obaid Siddiqis Lab',
 		description: 'Deepti Trivedi shares her memories of discussions between Obaid Siddiqi and Sunil Prabhakar, who joined the Siddiqi lab as an assistant, offering technical assistance for fly behavior experiments, stock keeping and management of laboratory consumables. ',
-		urls: [
-		'content/1/deepti_labtour-prabhakar_INTERSECTION-STAFF.mp3',
-		'content/1/OS Lab - Sunil Prabhakar citation.jpg'
-		] 
+		urls: {
+		audio: 'content/1/deepti_labtour-prabhakar_INTERSECTION-STAFF.mp3',
+		img: ['content/1/OS Lab - Sunil Prabhakar citation.jpg']
+	}
+		 
 	},
 	{
 		id: 'electrode',
 		title: 'Old electrode puller',
 		description: 'Electrode puller probably obtained from TIFR when Obaid Siddidi arrived at NCBS. It was never used and used to be in a room with other old equipment. Deepti Trivedi, a former student, was really surprised to see it outside and wonder if it was used in the later years, especially since the group had a completely functional Sutter electrode puller.',
-		urls: [
-		'content/2/deepti_labtour-electrode-heat_SPACE-TOUR.mp3',
-		'content/2/OS Lab - Nozzle - 1.jpg',
+		urls: {
+		audio: 'content/2/deepti_labtour-electrode-heat_SPACE-TOUR.mp3',
+		img: ['content/2/OS Lab - Nozzle - 1.jpg',
 		'content/2/OS Lab - Nozzle - 3.jpg',
-		'content/2/OS Lab - Nozzles.jpg'
-		] 
+		'content/2/OS Lab - Nozzles.jpg']
+	}
+		
 	},
 	{
 		id: 'meetings',
 		title: 'Lab Meetings',
 		description: 'Obaid Siddiqi’s lab meetings. Deepti Trivedi finds objects that she remembers from her lab meetings between 1999-2003.',
-		urls: [
-		'content/2/deepti_labtour-coasters_SPACE-TOUR.mp3',
-		'content/2/OS Lab - Coasters.jpg',
+		urls: {
+		audio: 'content/2/deepti_labtour-coasters_SPACE-TOUR.mp3',
+		img: ['content/2/OS Lab - Coasters.jpg',
 		'content/2/OS Lab - Lab Meet whiteboard.jpg',
-		'content/2/OS Lab - Table.jpg'
-		] 
+		'content/2/OS Lab - Table.jpg']
+	}
+		 
 	},
 	{
 		id: 'graphs',
 		title: 'Hand drawn graphs and verification of data',
 		description: 'Obaid Siddiqi avoided computer-generated graphs as far as possible and expected his students to do the same. He would force them to draw curves by hand to get a feel of the trend in a particular experiment. The philosophy of verifying your own data extended to other aspects of research, as shared by Deepti Trivedi',
-		urls: [
-		'content/3/deepti_labtour-graph-sequence-spit_RESEARCH-PROCESS.mp3',
-		'content/3/OS Lab - Hand drawn graphs.jpg'
-		] 
+		urls: {
+		audio: 'content/3/deepti_labtour-graph-sequence-spit_RESEARCH-PROCESS.mp3',
+		img: ['content/3/OS Lab - Hand drawn graphs.jpg']
+	}
+		
 	},
 	{
 		id: 'flood',
 		title: 'The 1999 flood',
 		description: 'A flood in 1999 in the Siddiqi lab led to water above the ankles, and damaging a lot of previous material. Deepti Trivedi, a former student, recounts the event.',
-		urls: [
-		'content/3/deepti_labtour-flood_SPACE-TOUR.mp3'
-		] 
+		urls: {
+		audio: 'content/3/deepti_labtour-flood_SPACE-TOUR.mp3'
+	}
+		 
 	},
 	{
 		id: 'benches',
 		title: 'Student Lab benches',
 		description: 'The lab bench of Mohammed bin Abu Baker, Obaid Siddiqi`s last PhD student at NCBS',
-		urls: [
-		'content/4/OS Lab - Abu bench.jpg'
-		] 
+		urls: {
+		img: ['content/4/OS Lab - Abu bench.jpg']
+	}
+		 
 	},
 	{
 		id: 'maze',
 		title: 'Fly maze apparatus',
 		description: 'T-traps were used for odour preference assays for Drosophila. One of the arms of the T-trap has an odorant while the other one is a control. Flies are let in from the third arm and allowed to roam around. The flies that are attracted by the specific odor (either because of innate preference or because of pre-conditioning) will go towards that arm. The trap is made in a way that once the fly enters one arm it is difficult to come out. At the end of the assay, the total number of flies in each arm is counted. The principle is same as Y-maze, but this assay proved to be less cumbersome and time consuming. Deepti Trivedi narrates some memories from her time in the lab.',
-		urls: [
-		'content/4/deepti_labtour-fly-maze-glass_SPACE-TOUR.mp3',
-		'content/4/OS Lab - Behavior - Maze - 1.jpg',
+		urls: {
+		audio: 'content/4/deepti_labtour-fly-maze-glass_SPACE-TOUR.mp3',
+		img: ['content/4/OS Lab - Behavior - Maze - 1.jpg',
 		'content/4/OS Lab - Behavior - Maze - 2.jpg',
-		'content/4/OS Lab - Behavior - Maze - 3.jpg'
-		] 
+		'content/4/OS Lab - Behavior - Maze - 3.jpg']
+	}
+		
 	},
 	{
 		id: 'spaces',
 		title: 'Lab Spaces',
 		description: 'Glimpses of Obaid Siddiqi`s lab.',
-		urls: [
-		'content/4/OS Lab - Chemical bench.jpg',
-		'content/4/OS Lab - Microscopes.jpg'
-		] 
+		urls: {
+		img: ['content/4/OS Lab - Chemical bench.jpg',
+		'content/4/OS Lab - Microscopes.jpg']
+	}
+		 
 	}
 
 ];
